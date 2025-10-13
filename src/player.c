b4d6f9ce62a83042060834e0ba89078869e7c12d
Azyor19 <118967712+Azyor19@users.noreply.github.com>
2025-10-14 00:07:35 +0200

    Update player.c
    Ajout de quelque primitives

--- src/player.c
+++ src/player.c
@@ -2,10 +2,46 @@
 #include "assets.h"
 #include "sprite.h"
 
+#define INVINSIBILITY_DURATION 10
+
 typedef struct {
-  int x,y;
-  int speed, hp;
+  Vector2 location; //Corner Up-Left for drawing
+  float speed; 
+  int hp;
+  Sprite sprite;
+  int invinsibility; //nombre de frames d'invinsibilité avant que le joueur ne puisse prendre des dégâts
 
-  SPRITE sprite;
-  
+} Player;
 
+int movePlayer(Player *player, Vector2 movement){
+//bouge le joueur d'un vecteur2 
+  (player->location).x += movement.x;
+  (player->location).y += movement.y;
+    //il doit bien avoir une fonction pour additionner des vecteurs plus facilement mais bon trkl
+}
+
+void initPlayer(Player *p, Vector2 location, float speed, int hp, Sprite sprite){
+  p->location = location;
+  p->speed = speed;
+  p->hp = hp;
+  p->sprite = sprite;
+  p->invisibility = 0;
+}
+
+
+int damagePlayer(Player *p, int damage){
+//inflige des dégâts au joueur et le rend invinsible. By-pass l'invinsibilité
+//renvoie 0 si le joueur est tué
+  p->hp -= damage;
+  if (p->hp <= 0)
+      return 0;
+  else{
+      p->invisibility = INVINSIBILITY_DURATION; 
+      return 1;
+  }
+}
+
+
+int playerIsAlive(Player p){
+    return (p.hp>0);
+}
