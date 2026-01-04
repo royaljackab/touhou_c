3b705a477fc6b0b96cc511cf04ee864cdba2f05d
jack <jackabribat@gmail.com>
2026-01-04 22:09:16 +0100

    task

--- src/moonlight.c
+++ src/moonlight.c
@@ -1,51 +1,51 @@
-#include "moonlight.h"
+// #include "moonlight.h"
 
-#define bossX objects[bossID].pos.x
-#define bossY objects[bossID].pos.y
+// #define bossX objects[bossID].pos.x
+// #define bossY objects[bossID].pos.y
 
-static ObjID bossID;
-static int timer=0;
+// static ObjID bossID;
+// static int timer=0;
 
-void fireRing() {
-    float angleT = GetRandomValue(0,360); 
-    for(int i=0; i<48; i++) {
-        CreateShot(bossX, bossY, 4.5, angleT, BALL_M_BLACK, 0);
-        angleT += 360.0/48;
-    }
-}
+// void fireRing() {
+//     float angleT = GetRandomValue(0,360); 
+//     for(int i=0; i<48; i++) {
+//         CreateShot(bossX, bossY, 4.5, angleT, BALL_M_BLACK, 0);
+//         angleT += 360.0/48;
+//     }
+// }
 
-void fireLaser(int dir) {
-    ObjID obj = CreateLaser(bossX,bossY,180 + 105*dir,1000,15,120, RED_LASER, 30);
-    ObjMove_AddPattern(obj, 30, NO_CHANGE, NO_CHANGE, NO_CHANGE, NO_CHANGE, 0.5 * dir);
-}
+// void fireLaser(int dir) {
+//     ObjID obj = CreateLaser(bossX,bossY,180 + 105*dir,1000,15,120, RED_LASER, 30);
+//     ObjMove_AddPattern(obj, 30, NO_CHANGE, NO_CHANGE, NO_CHANGE, NO_CHANGE, 0.5 * dir);
+// }
 
-void moonlight_init() {
-    bossID = Obj_Create(OBJ_BOSS);
+// void moonlight_init() {
+//     bossID = Obj_Create(OBJ_BOSS);
 
-    //texture du boss
-    Obj_SetTexture(bossID, BOSS_SPRITESHEET);
-    ObjSprite2D_SetSourceRect(bossID, 16,187,512,84);
-    ObjSprite2D_SetAnimation(bossID, 8, 5);
-    ObjSprite2D_SetCollisionToShot(bossID, 20);
+//     //texture du boss
+//     Obj_SetTexture(bossID, BOSS_SPRITESHEET);
+//     ObjSprite2D_SetSourceRect(bossID, 16,187,512,84);
+//     ObjSprite2D_SetAnimation(bossID, 8, 5);
+//     ObjSprite2D_SetCollisionToShot(bossID, 20);
 
 
-    ObjMove_SetPosition(bossID, 360, 200);
-}
+//     ObjMove_SetPosition(bossID, 360, 200);
+// }
 
-void moonlight() {
-    if (timer % 360 == 0) {
-        ObjMove_SetDestAtFrame(bossID, GetRandomValue( 30, 500), 200 + GetRandomValue(-20,50), 60);
-    }
-    if (timer > 60 && timer % 30 == 0) {
-        fireRing();
-    }
-    if (timer > 60 && timer % 360 == 90) {
-        fireLaser(1); fireLaser(-1);
-    }
+// void moonlight() {
+//     if (timer % 360 == 0) {
+//         ObjMove_SetDestAtFrame(bossID, GetRandomValue( 30, 500), 200 + GetRandomValue(-20,50), 60);
+//     }
+//     if (timer > 60 && timer % 30 == 0) {
+//         fireRing();
+//     }
+//     if (timer > 60 && timer % 360 == 90) {
+//         fireLaser(1); fireLaser(-1);
+//     }
 
-    timer++;
+//     timer++;
 
-}
+// }
 
 
 
---
+++ src/test_task.h
@@ -0,0 +1,6 @@
+#include "task.h"
+#include "player.h"
+#include "bullet_shot.h"
+
+void initialize();
+void moonlight_task(Task_contParam);
